a151955798284a5a9113480f69be7f7401c2e33a
riki-kraus <maof5728@gmail.com>
2025-06-03 10:04:26 +0300

    rem-env-angular

--- check-point-client/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    // production: false,
-    apiUrl:  `https://localhost:50397/api`,
-    googleClientId: '95515128712-njsrh6ah7f90pvtf24rld5b74tca65ke.apps.googleusercontent.com'
-  };
-  
